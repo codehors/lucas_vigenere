13c294338dcba099a53b107d452f3cf3e0cd206f
***CristaL <cristal@cristalcorp.com>
2016-09-29 05:06:52 +0200

    added some comments

--- lucas.c
+++ lucas.c
@@ -1,19 +1,32 @@
+/***********************
+ *                     *
+ * Vigenere Encryption *
+ *       09/29/16      *
+ *    Coded by Lucas   *
+ *        V 0.2        * 
+ *                     *
+ ***********************/
+
 #include <cs50.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-// What's up cristal? I'm sure this is full of inefficiencies but as is, it works :) 
+
 int main(int argc, string argv[])
 {
+    // checking arguments count
     if (argc != 2)
     {
         printf("Add one keyword on command line as key please!\n");
         return 1;
     }
     
+    // storing argument value
     string key = argv[1];
+
+    // and size
     int keylength = strlen(argv[1]);
     
     // Make sure it's all letters...
@@ -26,6 +39,8 @@ int main(int argc, string argv[])
         }
     }
     
+    // input message to encode
+    printf("Don't be a pangolin, input your message\n");
     string message = GetString();
 
     // array size starts at 0. string length starts at 1. ex : strlength("merde") will return 5, so you need a char array[4] (array[0] = "m", array[4] = "e")
@@ -35,10 +50,13 @@ int main(int argc, string argv[])
     // Counter for non-alphabetic characters so the key isn't used on them
     int nonalpha = 0;
     
+    // crypto loop start
     for (int i = 0, n = strlen(message); i < n; i++)
     {
+	// encoding upper characters    
         if (isupper(message[i]))
         {
+	    // 
             if (isupper(key[i % keylength]))
             {
                 code[i] = message[i] + key[(i - nonalpha) % keylength] - 'A';
@@ -46,6 +64,8 @@ int main(int argc, string argv[])
                 if ((message[i] + key[(i - nonalpha) % keylength] - 'A') > 'Z')
                     code[i] = code[i] - 26;
             }
+
+	    // 
             else
             {
                 code[i] = message[i] + key[(i - nonalpha) % keylength] - 'a';
@@ -54,8 +74,11 @@ int main(int argc, string argv[])
                     code[i] = code[i] - 26;
             }
         }
+
+	// encoding lower characters
         else if (islower(message[i]))
         {
+	    // 
             if (isupper(key[i % keylength]))
             {
                 code[i] = message[i] + key[(i - nonalpha) % keylength] - 'A';
@@ -63,6 +86,8 @@ int main(int argc, string argv[])
                 if ((message[i] + key[(i - nonalpha) % keylength] - 'A') > 'z')
                     code[i] = code[i] - 26;
             }
+
+	    // 
             else
             {
                 code[i] = message[i] + key[(i - nonalpha) % keylength] - 'a';
@@ -71,15 +96,18 @@ int main(int argc, string argv[])
                     code[i] = code[i] - 26;
             }
         }
+
+	// no up no low, increment non alpha counter
         else
         {
             code[i] = message[i];
             nonalpha++;
         }
         
+    // crypto loop end
     }
     
-    // Important part cris: I would have used normal printf for string "code" but weird characters kept on showing up. Not sure how to fix.
+    // Important part cristal: I would have used normal printf for string "code" but weird characters kept on showing up. Not sure how to fix.
     /*for (int i = 0, n = strlen(message); i < n; i++)
         printf("%c", code[i]);*/
     printf("%s \n", code);
